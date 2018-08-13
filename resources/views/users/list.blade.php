dcee5e4b1b7733ddf9bc12d832dc7909caf920ab
Naren <developer.naren@gmail.com>
2018-08-13 22:52:10 +0545

    added user list

---
+++ resources/views/users/list.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>
+        User List
+    </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <h1>User List</h1>
+        <table class="table">
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+            </tr>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+</body>
+</html>
